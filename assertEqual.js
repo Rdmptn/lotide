0e23dc5fceaae2fbe7f4a23e5e1d5aa7bdbbdd38
Cameron <cameron_uniac@hotmail.com>
2021-06-21 13:26:16 -0400

    Wrote test code for evaluating comparisons

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🍀🍀🍀 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
