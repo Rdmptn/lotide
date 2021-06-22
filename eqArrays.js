c96d6f6496aab3e0f9485644a796a025a130363a
Cameron <cameron_uniac@hotmail.com>
2021-06-22 13:58:46 -0400

    Created array comparator function 'eqArrays'

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArrays =  function(arr1, arr2) {
+  for (let i =0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+      
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
