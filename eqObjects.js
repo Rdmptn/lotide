f268ac273f8d214e92e70a222fce0ddf2ec4b088
Cameron <cameron_uniac@hotmail.com>
2021-06-23 15:40:45 -0400

    Removed test code

--- eqObjects.js
+++ eqObjects.js
@@ -24,10 +24,8 @@ const eqObjects = function(object1, object2) {
     return false;
   } else {
     for (let key in object1) {
-      console.log(`Object 1 key: ${key}, value: ${object1[key]}`)
       console.log(`Object 2 key: ${key}, value: ${object2[key]}`)
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        console.log("arrays!");
         if (eqArrays(object1[key], object2[key]) !== true) {
           return false;
         }
