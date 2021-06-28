726587394d2f18af8d7ac63842c077803062497f
Cameron <cameron_uniac@hotmail.com>
2021-06-28 14:50:40 -0400

    Refactored test code to work with mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
 // Test Cases
-assertEqual(tail([5,6,7]), 7);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+
+// assertEqual(tail([5,6,7]), 7);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  
+   it("returns 7 for [5, 6, 7]", () => {
+    assert.strictEqual(tail([5, 6, 7]), 7); 
+  });
+  
+  it('returns "Labs" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+  });
+});
