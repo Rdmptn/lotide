6ea8420c648c2d16660c9b4634619f34ec572f9d
Cameron <cameron_uniac@hotmail.com>
2021-06-28 13:58:01 -0400

    Refactored old files to support module exporting and separated test cases into new files

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr[0];
 };
 
-// Test Cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
