433e2df534c7a0aa1f679d6d93175446e378be0c
Cameron <cameron_uniac@hotmail.com>
2021-06-28 14:05:10 -0400

    Removed assertEqual function, imported via require instead

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require.('./assertEqual');
 
 const tail = function(arr) {
   return arr[arr.length - 1];
