70c2d8d0b03142759dd70611ca9557139217dfce
Cameron <cameron_uniac@hotmail.com>
2021-06-28 16:00:15 -0400

    Fixed tail function and updated test cases to match

--- test/tailTest.js
+++ test/tailTest.js
@@ -8,14 +8,14 @@ const assert = require('chai').assert;
 
 describe("#tail", () => {
   it("returns 3 for [1, 2, 3]", () => {
-    assert.strictEqual(tail([1, 2, 3]), 3);
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
   
-   it("returns 7 for [5, 6, 7]", () => {
-    assert.strictEqual(tail([5, 6, 7]), 7); 
+   it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]); 
   });
   
-  it('returns "Labs" for ["Hello", "Lighthouse", "Labs"]', () => {
-    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+  it('returns "["Lighthouse", "Labs"]" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 });
